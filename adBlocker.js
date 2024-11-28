e220543df10f73319232c94d3fbaf7cf4bd108c8
Kushal Shah <dev@kushalshah.com.np>
2024-11-28 19:00:47 +0545

    some changes made

--- adBlocker.js
+++ adBlocker.js
@@ -13,7 +13,6 @@ function removeAds() {
     }
 
     if (/https:\/\/www\.youtube\.com\/watch\?.*/.test(currentURL)) {
-        // Select elements that may contain ads
         const adShowing = document.querySelector('.ad-showing');
         const ad2Showing = document.querySelector('#video-ads');
         const bannerShowing = document.querySelector('.player-ads');
@@ -21,12 +20,14 @@ function removeAds() {
         const skipButtonShowing = document.querySelector('#ytp-ad-skip-button-modern');
         const adContainerShowing = document.querySelector('.ytp-cultural-moment-player-content');
         const blockMessageShown = document.querySelector('ytd-enforcement-message-view-model');
+        const premiumDialogShowing = document.querySelector('.mealbar-promo-renderer');
+        const button = document.querySelector('efyt-not-interested');
 
-        // Remove ads if they are found
         removeElement(bannerShowing);
         removeElement(ad2Showing);
         removeElement(adMiniBanner);
         removeElement(adContainerShowing);
+        removeElement(premiumDialogShowing?.parentNode);
         clickElement(skipButtonShowing);
 
         if (blockMessageShown) {
@@ -37,6 +38,17 @@ function removeAds() {
         if (blockCounter > 10) {
             handleAdBlockerRetry();
         }
+
+        if (button) {
+            button.click();
+            failCounter = 0;
+        } else {
+            handleButtonNotFound();
+        }
+
+        if (failCounter > 10) {
+            handleFailedToFindButton();
+        }
     }
 
     if (/https:\/\/www\.youtube\.com\/$/.test(currentURL)) {
@@ -45,6 +57,7 @@ function removeAds() {
         const ytAdBanner = document.querySelector('ytd-statement-banner-renderer');
 
         removeElement(headAdShowing);
+
         if (adCardShowing || ytAdBanner) {
             const adParent = (adCardShowing || ytAdBanner).parentNode.parentNode;
             removeElement(adParent);
@@ -73,6 +86,21 @@ function handleAdBlockerRetry() {
     const blocker = window.confirm("Make sure there are no other ad-blockers working on this page.\n\nPress 'OK' to retry.\nPress 'Cancel' to disable the YouTube ad-blocker script for this session.");
     if (blocker) {
         blockCounter = 0;
+    } else {
+        masterSwitch = false;
+    }
+}
+
+function handleButtonNotFound() {
+    console.error(`Failed to find button. Retrying in ${searchInterval} ms`);
+    failCounter++;
+}
+
+function handleFailedToFindButton() {
+    const buttonNotFound = window.confirm("Failed to find the 'Remove Ads' button. Please make sure that Enhancer for YouTube is installed.\n\nPress 'OK' to redirect to the installation page.\nPress 'Cancel' to disable the YouTube ad-blocker script for this session.");
+    if (buttonNotFound) {
+        window.open("https://chrome.google.com/webstore/detail/enhancer-for-youtube/ponfpcnoihfmfllpaingbgckeeldkhle");
+        failCounter = 0;
     } else {
         masterSwitch = false;
     }
